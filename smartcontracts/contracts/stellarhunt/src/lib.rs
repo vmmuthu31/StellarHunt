f555f35e6ef7c8103e7516e95f1ee935da175529
Hema Devi <uhemadevi1927@gmail.com>
2024-07-10 11:08:42 +0530

    basic player contract

--- smartcontracts/contracts/stellarhunt/src/lib.rs
+++ smartcontracts/contracts/stellarhunt/src/lib.rs
@@ -1,12 +1,33 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, vec, Address, Env, Symbol, Vec};
 
 #[contract]
-pub struct HelloContract;
+
+const INITIAL_HEALTH: u32 = 100;
+const INITIAL_POINTS: u32 = 100;
+const INITIAL_CRYSTALS: u32 = 0;
+const INITIAL_WEAPONS: u32 = 1;
+
+pub struct Player {
+    pub health: u32,
+    pub points: u32,
+    pub crystals: u32,
+    pub weapons: Vec<u32>,
+    pub matches_played: u32,
+}
+pub struct StellarHunt;
 
 #[contractimpl]
-impl HelloContract {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        vec![&env, symbol_short!("Hello"), to]
+impl StellarHunt {
+    pub fn initialize_player(env: Env, player_address: Address) {
+        let player = Player {
+            health: INITIAL_HEALTH,
+            points: INITIAL_POINTS,
+            crystals: INITIAL_CRYSTALS,
+            weapons: vec![INITIAL_WEAPONS],
+            matches_played: 0,
+        };
+
+        env.storage().set(&player_address, &player);
     }
 }
