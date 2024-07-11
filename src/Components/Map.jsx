e325489b020e2ef4b366b666e1696ba9efcf5dcc
Vairamuthu M <mvairamuthu2003@gmail.com>
2024-07-12 01:00:10 +0530

    updated the map model

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -3,21 +3,22 @@ import { RigidBody } from "@react-three/rapier";
 import { useEffect } from "react";
 
 export const Map = () => {
-  const map = useGLTF("models/vm.glb");
+  const { scene } = useGLTF("models/map.glb");
+
   useEffect(() => {
-    map.scene.traverse((child) => {
+    scene.traverse((child) => {
       if (child.isMesh) {
         child.castShadow = true;
         child.receiveShadow = true;
       }
     });
-  });
+  }, [scene]);
+
   return (
-    <>
-      <RigidBody colliders="trimesh" type="fixed">
-        <primitive object={map.scene} />
-      </RigidBody>
-    </>
+    <RigidBody colliders="trimesh" type="fixed">
+      <primitive object={scene} />
+    </RigidBody>
   );
 };
-useGLTF.preload("models/vm.glb");
+
+useGLTF.preload("models/map.glb");
